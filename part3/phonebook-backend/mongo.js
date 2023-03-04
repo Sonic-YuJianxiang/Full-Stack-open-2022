5cc2b3b8803df73270a07f7b546d60a729bb1da8
Sonic-YuJianxiang <1347256906@qq.com>
2023-03-04 19:12:56 +0200

    Exericse 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const personName = process.argv[3]
+const personNumber = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.6g93c7v.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    content: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    content: personName,
+    number: personNumber,
+})
+
+if (process.argv.length === 3) {
+    console.log('phonebook:')
+    Person.find({}).then(persons => {
+        persons.forEach(person => {
+            console.log(`${person.content} ${person.number}`)
+        });
+        mongoose.connection.close()
+    })
+} else if (process.argv.length === 5) {
+        person.save().then(result => {
+        console.log(`added ${personName} number ${personNumber} to phonebook`)
+        mongoose.connection.close()
+    })
+} else{
+    console.log('Please type correct command like: node mongo.js <password> to display all persons in phonebook or node mongo.js <password> <name> <number>')
+    process.exit()
+}
