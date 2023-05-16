191572ce475b4d39966af383120ce85971fd8468
Sonic-YuJianxiang <1347256906@qq.com>
2023-05-16 11:19:10 +0300

    finnish part 4.5 - 4.7

--- part4/BlogList/tests/list_helper.test.js
+++ part4/BlogList/tests/list_helper.test.js
@@ -9,20 +9,85 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
-    test('when there is no blogs', () => {
-        expect(totalLikes([])).toBe(0)
-      })
-    
-      test('when there is only one blog', () => {
-        expect(totalLikes([{likes: 1}])).toBe(1)
-      })
-    
-      test('when there are several blogs', () => {
-        const blogs = [
-          {likes: 1},
-          {likes: 3},
-          {likes: 4}
-        ]
-        expect(totalLikes(blogs)).toBe(8)
+  test('when there is no blogs', () => {
+      expect(totalLikes([])).toBe(0)
     })
+  
+    test('when there is only one blog', () => {
+      expect(totalLikes([{likes: 1}])).toBe(1)
+    })
+  
+    test('when there are several blogs', () => {
+      const blogs = [
+        {likes: 1},
+        {likes: 3},
+        {likes: 4}
+      ]
+      expect(totalLikes(blogs)).toBe(8)
+  })
+})
+
+describe('favorite blog', () => {
+  const singleBlog = [{likes: 9}]
+  const blogs = [
+    {likes:9},
+    {likes:8},
+    {likes:10}
+  ]
+
+  test('when there is no blogs', () => {
+    expect(favoriteBlog([])).toEqual({})
+  })
+
+  test('when there is only one blog', () => {
+    expect(favoriteBlog(singleBlog)).toEqual({likes: 9})
+  })
+
+  test('when there are several blogs', () => {
+    expect(favoriteBlog(blogs)).toEqual({likes:10})
+  })
+})
+
+describe('most blogs', () => {
+  const singleBlog = [{author:'Beth'}]
+  const blogs = [
+    {author: 'Layla'},
+    {author: 'Nick'},
+    {author: 'Nick'}
+  ]
+
+  test('when there is no blogs', () => {
+    expect(mostBlogs([])).toEqual({})
+  })
+
+  test('when there is a single blog', () => {
+    expect(mostBlogs(singleBlog)).toEqual({author: 'Beth', blogs: 1})
+  })
+
+  test('when there are several blogs with several authors', () => {
+    expect(mostBlogs(blogs)).toEqual({author: 'Nick', blogs: 2})
+  })
+})
+
+describe('most likes', () => {
+
+  const singleAuthor = [{author:'Beth', likes: 9}]
+  const blogs = [
+    {author: 'Layla', likes: 8},
+    {author: 'Nick', likes: 10},
+    {author: 'Nick', likes: 8},
+    {author: 'Layla', likes: 9}
+  ]
+
+  test('when there are no blogs', () => {
+    expect(mostLikes([])).toEqual({})
+  })
+
+  test('when there is a single blog', () => {
+    expect(mostLikes(singleAuthor)).toEqual({author:'Beth', likes: 9})
+  })
+
+  test('when there are several blogs', () => {
+    expect(mostLikes(blogs)).toEqual({author: 'Nick', likes: 18})
+  })
 })
--- part4/BlogList/utils/list_helper.js
+++ part4/BlogList/utils/list_helper.js
@@ -12,8 +12,61 @@ const totalLikes = (blogs) => {
     }
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return {}
+    } else if (blogs.length === 1) {
+        return {...blogs[0]}
+    } else {
+        const favorite = blogs.reduce((prev, current) => { prev.likes > current.likes ? prev : current })
+        return {title: favorite.title, author: favorite.author, likes: favorite.likes}
+    }
+}
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+      return {}
+    } else {
+      const reducedBlogs = blogs
+        .map(blog => blog.author)
+        .reduce((author, val) => {
+          author[val] = (author[val] || 0) + 1
+          return author
+        }, {})
+  
+      const mostBlogsAuthor =
+          Object.keys(reducedBlogs)
+            .map(key => ({ author:key, blogs:reducedBlogs[key] }))
+            .reduce((prev, current) => (prev.blogs > current.blogs) ? prev : current)
+  
+      return mostBlogsAuthor
+    }
+  }
+  
+const mostLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return {}
+    } else {
+
+        const reducedBlogs = blogs
+        .reduce((acc, blog) => ({
+            ...acc,
+            [blog.author]: (acc[blog.author] || 0) + blog.likes
+        }), {})
+
+        const mostLikesAuthor =
+            Object.keys(reducedBlogs)
+            .map(key => ({ author: key, likes:reducedBlogs[key] }))
+            .reduce((prev, current) => (prev.likes > current.likes) ? prev : current)
+
+        return mostLikesAuthor
+}
+}
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
